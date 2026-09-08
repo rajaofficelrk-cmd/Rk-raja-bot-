2e70c30d9d23f638f1f419da30b2939152f387cb
rajaofficelrk-cmd <rajaofficelrk@gmail.com>
2026-09-09 00:52:23 +0530

    Update zuck.js

--- Ayush/commands/zuck.js
+++ Ayush/commands/zuck.js
@@ -5,7 +5,7 @@ module.exports.config = {
 
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "◄⸻̅͟ˣ͠𓆩𝐑꯭꘍꯭֟፝͡᪂꘍꯭ 𝐋꯭𖾝ԍ𖾝꯭֟፝͡᎔꯭𑀘𓆪꯭ˣ͢",
 	description: "Comment on the board ( ͡° ͜ʖ ͡°)",
 	commandCategory: "edit-img",
 	usages: "zuck [text]",
