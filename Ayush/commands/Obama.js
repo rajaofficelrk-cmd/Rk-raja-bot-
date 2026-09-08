c52eef868b8fb0615477a0b36bc48f846429041b
rajaofficelrk-cmd <rajaofficelrk@gmail.com>
2026-09-09 00:47:04 +0530

    Update Obama.js

--- Ayush/commands/Obama.js
+++ Ayush/commands/Obama.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "obama",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "◄⸻̅͟ˣ͠𓆩𝐑꯭꘍꯭֟፝͡᪂꘍꯭ 𝐋꯭𖾝ԍ𖾝꯭֟፝͡᎔꯭𑀘𓆪꯭ˣ͢",
 	description: "Obama Tweet post",
 	commandCategory: "edit-img",
 	usages: "[text]",
