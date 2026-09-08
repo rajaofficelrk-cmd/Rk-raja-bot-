430c957bc139b0c858dccff976c7526f0d731f4a
rajaofficelrk-cmd <rajaofficelrk@gmail.com>
2026-09-09 00:54:40 +0530

    Update Work-beta.js

--- Ayush/commands/Work-beta.js
+++ Ayush/commands/Work-beta.js
@@ -3,7 +3,7 @@ module.exports.config = {
     name: "job",
     version: "1.0.2",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
+    credits: "◄⸻̅͟ˣ͠𓆩𝐑꯭꘍꯭֟፝͡᪂꘍꯭ 𝐋꯭𖾝ԍ𖾝꯭֟፝͡᎔꯭𑀘𓆪꯭ˣ͢", 
     description: "",
     commandCategory: "Economy",
     cooldowns: 5,
